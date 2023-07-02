048813976137b8ca14506d92b63795e81a86af34
Pedro Henrique <phandradesa@gmail.com>
2023-07-02 17:57:52 -0300

    🚧 Trabalho React - Pokemon - voltando para home

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,11 +1,14 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
-
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import { useNavigate } from "react-router-dom";
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -50,7 +53,8 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function Navbar({ filterPokemon, searchHide }) {
-
+ 
+  const navigate = useNavigate();
   return (
     <Box sx={{ flexGrow: 1, marginBottom: "1em" }}>
       <AppBar position="static" sx={{ backgroundColor: "black" }}>
@@ -62,7 +66,8 @@ export default function Navbar({ filterPokemon, searchHide }) {
               component="img"
               src="/img/pokemon/pokemon-logo.png"
               height="3em"
-              x={{ cursor: "pointer" }}
+             sx={{ cursor: "pointer" }} onClick={() => navigate("/")}
+ 
             />
             {!searchHide && (
               <Search onChange={(event) => filterPokemon(event.target.value)}>
